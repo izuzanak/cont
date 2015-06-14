29da1e5849e344112f051afdf4ab920e1e2e38d5
Jiří Zuzaňák <zuzanakjiri@gmail.com>
2015-06-14 18:37:05 +0200

    added int_str_map example

---
+++ cont/examples/08_int_str_map/int_str_map.h
@@ -0,0 +1,418 @@
+
+#ifndef __DYNAMIC_H
+#define __DYNAMIC_H
+
+@begin
+include "struct.h"
+@end
+
+#include <stdarg.h>
+
+/*
+ * constants and definitions
+ */
+
+// - string terminating character -
+extern const char c_string_terminating_char;
+
+// - tabulator size -
+const unsigned c_tabulator_size = 8;
+
+/*
+ * definition of structure string_s
+ */
+
+@begin
+define string_s dynamic
+@end
+
+struct string_s
+{
+  unsigned size;
+  char *data;
+
+  /*!
+   * \brief initialization of string variables
+   */
+  inline void init();
+
+  /*!
+   * \brief release of string variables
+   */
+  inline void clear();
+
+  /*!
+   * \brief create new string
+   * \param a_length - length of new string
+   */
+  inline void create(unsigned a_length);
+
+  /*!
+   * \brief set string to given data
+   * \param a_length - length of new string
+   * \param a_data - pointer to data of new string
+   */
+  inline void set(unsigned a_length,const char *a_data);
+
+  /*!
+   * \brief create new string by concatenation of two strings described by length and pointer
+   * \param a_flength - length of first string
+   * \param a_fdata - pointer to data of first string
+   * \param a_slength - length of second string
+   * \param a_sdata - pointer to data of second string
+   */
+  inline void conc_set(unsigned a_flength,const char *a_fdata,unsigned a_slength,const char *a_sdata);
+
+  /*!
+   * \brief flush all variables of string
+   */
+  inline void flush_all() {}
+
+  /*!
+   * \brief swap string variables with another string
+   * \param a_second - reference to another string
+   */
+  inline void swap(string_s &a_second);
+
+  /*!
+   * \brief select one character from string
+   * \param a_idx - index of selected character
+   * \return reference to character
+   */
+  inline char &operator[](unsigned a_idx);
+
+  /*!
+   * \brief set string to value of another string
+   * \param a_src - reference to another string
+   * \return reference to this string
+   */
+  inline string_s &operator=(string_s &a_src);
+
+  /*!
+   * \brief compare value with another string
+   * \param a_second - reference to another string
+   * \return result of comparison
+   */
+  inline bool operator==(string_s &a_second);
+
+  /*!
+   * \brief compare string with string described by its length and its data
+   * \param a_length - length of second string
+   * \param a_data - data of second string
+   * \return result of comparison
+   */
+  inline bool compare_char_ptr(unsigned a_length,const char *a_data);
+
+  /*!
+   * \brief multiply character string a_mult times
+   *
+   * \param a_length - length of string to be multiplied
+   * \param a_data - pointer to string data
+   * \param a_mult - multiplier of string
+   */
+  void mult_char_ptr(unsigned a_length,const char *a_data,unsigned a_mult);
+
+  /*!
+   * \brief convert utf8 string to utf32 unsigned int string
+   * \param a_src - pointer to source utf8 characters
+   * \param a_trg - pointer to target utf32 unsigned integers
+   * \param a_size - count of characters to be converted
+   * \return count of converted integers
+   */
+  static int utf8_to_utf32(char *a_src,unsigned *a_trg,unsigned a_size);
+
+  /*!
+   * \brief convert utf32 unsigned int string to utf8 string
+   * \param a_src - pointer to source utf32 unsigned integeres
+   * \param a_trg - pointer to target utf8 characters
+   * \param a_size - count of integers to be converted
+   * \return count of converted characters
+   */
+  static unsigned utf32_to_utf8(unsigned *a_src,char *a_trg,unsigned a_size);
+
+  /*!
+   * \brief print string to standard output
+   * \return count of writed characters
+   */
+  inline unsigned print();
+
+  /*!
+   * \brief load string from given stream
+   * \param stream - stream from which is string loaded
+   */
+  bool read_line_from_stream(FILE *stream);
+
+  /*!
+   * \brief load string from text file
+   * \param a_file - name of file containing string text
+   * \return true if string is successfully loaded
+   */
+  inline bool load_text_file(const char *a_file);
+
+  /*!
+   * \brief save string to text file
+   * \param a_file - name of target file
+   * \return true if string is successfully saved
+   */
+  inline bool save_text_file(const char *a_file);
+
+  /*!
+   * \brief create string of desired format
+   * \param format - format as in printf functions family
+   * \param ... - list of parameters desired in format
+   */
+  void setf(const char *format,...);
+
+  /*!
+   * \brief conctenate string of desired format
+   * \param format - format as in printf functions family
+   * \param ... - list of parameters demanded in format
+   */
+  void concf(const char *format,...);
+
+  /*!
+   * \brief find position of given string in string from given index
+   * \param a_idx - position from which is substring searched
+   * \param a_length - length of searched string
+   * \param a_data - data of searched string
+   */
+  unsigned get_idx(unsigned a_idx,unsigned a_length,const char *a_data);
+
+  /*!
+   * \brief return print size between two character indexes in string
+   * \param f_idx - index of first character
+   * \param s_idx - index of second character
+   * \return print size
+   */
+  unsigned get_print_size_between(unsigned f_idx,unsigned s_idx);
+
+  /*!
+   * \brief return number of line of character index
+   * \param c_idx - character index
+   * \return line number
+   */
+  unsigned get_character_line(unsigned c_idx);
+
+  /*!
+   * \brief return index of character at beginning of line on which is located given character
+   * \param c_idx - index of given character
+   * \return index of begin of line
+   */
+  unsigned get_character_line_begin(unsigned c_idx);
+
+  /*!
+   * \brief return index of character at end of line on which is located given character
+   * \param c_idx - index of given character
+   * \return index of end of line
+   */
+  unsigned get_character_line_end(unsigned c_idx);
+};
+
+/*
+ * definition of generated structures
+ */
+
+// -- ui_array_s --
+@begin
+   array<unsigned> ui_array_s;
+@end
+
+// -- int_string_s --
+@begin
+   struct
+   <
+   int:index
+   string_s:value
+   >
+   int_string_s;
+@end
+
+// -- int_string_map_s --
+@begin
+   rb_tree<int_string_s> int_string_map_s;
+@end
+
+/*
+ * inline methods of structure string_s
+ */
+
+inline void string_s::init()
+{/*{{{*/
+  size = 1;
+  data = (char *)&c_string_terminating_char;
+}/*}}}*/
+
+inline void string_s::clear()
+{/*{{{*/
+  if (data != &c_string_terminating_char)
+  {
+    cfree(data);
+  }
+
+  init();
+}/*}}}*/
+
+inline void string_s::create(unsigned a_length)
+{/*{{{*/
+  clear();
+  data = (char *)cmalloc((a_length + 1)*sizeof(char));
+
+  data[a_length] = '\0';
+  size = a_length + 1;
+}/*}}}*/
+
+inline void string_s::set(unsigned a_length,const char *a_data)
+{/*{{{*/
+  clear();
+  data = (char *)cmalloc((a_length + 1)*sizeof(char));
+
+  if (a_length != 0)
+  {
+    memcpy(data,a_data,a_length*sizeof(char));
+  }
+
+  data[a_length] = '\0';
+  size = a_length + 1;
+}/*}}}*/
+
+inline void string_s::conc_set(unsigned a_flength,const char *a_fdata,unsigned a_slength,const char *a_sdata)
+{/*{{{*/
+  clear();
+  data = (char *)cmalloc((a_flength + a_slength + 1)*sizeof(char));
+
+  if (a_flength != 0)
+  {
+    memcpy(data,a_fdata,a_flength*sizeof(char));
+  }
+
+  if (a_slength != 0)
+  {
+    memcpy(data + a_flength,a_sdata,a_slength*sizeof(char));
+  }
+
+  data[a_flength + a_slength] = '\0';
+  size = a_flength + a_slength + 1;
+}/*}}}*/
+
+inline void string_s::swap(string_s &a_second)
+{/*{{{*/
+  unsigned tmp_size = size;
+  size = a_second.size;
+  a_second.size = tmp_size;
+
+  char *tmp_data = data;
+  data = a_second.data;
+  a_second.data = tmp_data;
+}/*}}}*/
+
+inline char &string_s::operator[](unsigned a_idx)
+{/*{{{*/
+  debug_assert(a_idx < size - 1);
+  return data[a_idx];
+}/*}}}*/
+
+inline string_s &string_s::operator=(string_s &a_src)
+{/*{{{*/
+  clear();
+
+  if (a_src.data == &c_string_terminating_char) return *this;
+  data = (char *)cmalloc(a_src.size*sizeof(char));
+  memcpy(data,a_src.data,(a_src.size - 1)*sizeof(char));
+  data[a_src.size - 1] = '\0';
+  size = a_src.size;
+
+  return *this;
+}/*}}}*/
+
+inline bool string_s::operator==(string_s &a_second)
+{/*{{{*/
+  if (size != a_second.size) return false;
+  if (data == &c_string_terminating_char) return true;
+  return (memcmp(data,a_second.data,(size - 1)*sizeof(char)) == 0);
+}/*}}}*/
+
+inline bool string_s::compare_char_ptr(unsigned a_length,const char *a_data)
+{/*{{{*/
+  if (size != a_length + 1) return false;
+  if (a_length == 0) return true;
+  return (memcmp(data,a_data,a_length*sizeof(char)) == 0);
+}/*}}}*/
+
+inline unsigned string_s::print()
+{/*{{{*/
+  return fwrite(data,size - 1,1,stdout);
+}/*}}}*/
+
+inline bool string_s::load_text_file(const char *a_file)
+{/*{{{*/
+  if (a_file == NULL) return false;
+  FILE *f = fopen(a_file,"r");
+  if (f == NULL) return false;
+
+  fseek(f,0,SEEK_END);
+  unsigned file_size = ftell(f);
+  fseek(f,0,SEEK_SET);
+
+  clear();
+  data = (char *)cmalloc((file_size + 1)*sizeof(char));
+  if (fread(data,file_size,1,f) != 1)
+  {
+    fclose(f);
+    return false;
+  }
+  fclose(f);
+
+  data[file_size] = '\0';
+  size = file_size + 1;
+
+  return true;
+}/*}}}*/
+
+inline bool string_s::save_text_file(const char *a_file)
+{/*{{{*/
+  if (a_file == NULL) return false;
+  FILE *f = fopen(a_file,"w");
+  if (f == NULL) return false;
+
+  if (size > 1)
+  {
+    if (fwrite(data,size - 1,1,f) != 1)
+    {
+      fclose(f);
+      return false;
+    }
+  }
+
+  fclose(f);
+  return true;
+}/*}}}*/
+
+/*
+ * inline methods of generated structures
+ */
+
+// -- ui_array_s --
+@begin
+   inlines ui_array_s
+@end
+
+// -- int_string_s --
+@begin
+   inlines int_string_s
+@end
+
+// -- int_string_map_s --
+@begin
+   inlines int_string_map_s
+@end
+
+inline int int_string_map_s::__compare_value(int_string_s &a_first,int_string_s &a_second)
+{/*{{{*/
+  int &fi = a_first.index;
+  int &si = a_second.index;
+
+  return fi < si ? -1 : fi > si ? 1 : 0;
+}/*}}}*/
+
+#endif
+
